7424a8731e74b56de030989b56a3f9e820e12b96
Kerwin Yen <kevinyenhaha@gmail.com>
2022-08-29 02:45:32 +1000

    60 percent complete

---
+++ Assets/Scripts/LoadAssets.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssets : MonoBehaviour
+{
+    public GameObject redOb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
